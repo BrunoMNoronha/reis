856a4e453b0f1ab4ca24a3b9928a277846705db7
Bruno M. Noronha <bruno.developer@pm.me>
2025-06-06 06:54:41 -0300

    new version

--- javascript/modal.js
+++
@@ -1,9 +0,0 @@
-// Funções para modais
-export function showModal(modal) {
-    modal.style.display = 'flex';
-    const input = modal.querySelector('input, select, textarea');
-    if (input) input.focus();
-}
-export function hideModal(modal) {
-    modal.style.display = 'none';
-}
